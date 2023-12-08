2505f9ce69d1ec5e4e43c26afca05b5d26d4cb2e
Scotty Jamison <theScottyJam@gmail.com>
2023-12-08 09:29:01 -0700

    Add content and opinions to escape/unescape

--- content/nolodash/escape/test.js
+++ content/nolodash/escape/test.js
@@ -1,26 +1,24 @@
-describe('escape()', () => {
+function escapeHtmlChars(string) {
+  return string
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
+}
+
+describe('escapeHtmlChars()', () => {
   it('escapes all characters', () => {
-    const res = _.escape('&<>"\'');
+    const res = escapeHtmlChars('&<>"\'');
     expect(res).toEqual('&amp;&lt;&gt;&quot;&#39;');
   });
 
   it('escapes all instances', () => {
-    const res = _.escape('&<&<&');
+    const res = escapeHtmlChars('&<&<&');
     expect(res).toEqual('&amp;&lt;&amp;&lt;&amp;');
   });
 
-  it("doesn't panic on edge cases", () => {
-    // empty
-    expect(_.escape('')).toEqual('');
-    // undefined
-    expect(_.escape()).toEqual('undefined');
-    // null
-    expect(_.escape(null)).toEqual('null');
-    // Number
-    expect(_.escape(10)).toEqual('10');
-    // Array
-    expect(_.escape(['hello', 'world'])).toEqual('hello,world');
-    // Object
-    expect(_.escape({ obj: true })).toEqual('[object Object]');
+  it("doesn't panic on the empty string", () => {
+    expect(escapeHtmlChars('')).toEqual('');
   });
 });
--- content/nolodash/unescape/test.js
+++ content/nolodash/unescape/test.js
@@ -1,26 +1,33 @@
+function unescapeHtmlChars(string) {
+  // Unecoding "&" needs to be done last.
+  // If it is done first, something like "&amp;lt;" would incorrectly
+  // get unencoded to "<", when it should be "&lt;"
+  return string
+    .replaceAll('&lt;', '<')
+    .replaceAll('&gt;', '>')
+    .replaceAll('&quot;', '"')
+    .replaceAll('&#39;', "'")
+    .replaceAll('&amp;', '&');
+}
+
 describe('unescape()', () => {
   it('unescapes all characters', () => {
-    const res = _.unescape('&amp;&lt;&gt;&quot;&#39;');
+    const res = unescapeHtmlChars('&amp;&lt;&gt;&quot;&#39;');
     expect(res).toEqual('&<>"\'');
   });
 
   it('unescapes all instances', () => {
-    const res = _.unescape('&amp;&lt;&amp;&lt;&amp;');
+    const res = unescapeHtmlChars('&amp;&lt;&amp;&lt;&amp;');
     expect(res).toEqual('&<&<&');
   });
 
-  it("doesn't panic on edge cases", () => {
-    // empty
-    expect(_.unescape('')).toEqual('');
-    // undefined
-    expect(_.unescape()).toEqual('undefined');
-    // null
-    expect(_.unescape(null)).toEqual('null');
-    // Number
-    expect(_.unescape(10)).toEqual('10');
-    // Array
-    expect(_.unescape(['hello', 'world'])).toEqual('hello,world');
-    // Object
-    expect(_.unescape({ obj: true })).toEqual('[object Object]');
+  it("doesn't panic on the empty string", () => {
+    expect(unescapeHtmlChars('')).toEqual('');
+  });
+
+  it('does not double-unescape characters', () => {
+    const res = unescapeHtmlChars('&amp;lt;');
+    // Should equal "&lt;", not "<"
+    expect(res).toEqual('&lt;');
   });
 });
