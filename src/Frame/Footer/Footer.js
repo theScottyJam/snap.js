286d566cc105982919671884f2ad4ba245ce33d4
Scotty Jamison <theScottyJam@gmail.com>
2024-11-25 09:31:22 -0700

    Fix linting

--- src/Frame/Footer/Footer.js
+++ src/Frame/Footer/Footer.js
@@ -12,7 +12,10 @@ export default function Footer() {
           Github
         </a>
         .<br className={style.breakWhenSmall} /> All code presented is under the{' '}
-        <a href="https://github.com/theScottyJam/snap.js/blob/main/license" rel="noreferrer">
+        <a
+          href="https://github.com/theScottyJam/snap.js/blob/main/license"
+          rel="noreferrer"
+        >
           "unlicense" license
         </a>
         .
